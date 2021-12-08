503f9163a2e6646b1f7bc8bbe1d7794838defc22
CharbelMakhlouf <73894289+CharbelMakhlouf@users.noreply.github.com>
2021-12-08 20:53:06 +0200

    testing commit

--- Supermarket Application/src/supermarket/application/SupermarketApplication.java
+++ Supermarket Application/src/supermarket/application/SupermarketApplication.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,19 +21,7 @@ public class SupermarketApplication extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
+        Pane root = new Pane();
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("fadi");
