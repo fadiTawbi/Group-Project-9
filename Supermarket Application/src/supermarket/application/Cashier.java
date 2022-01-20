622e9320cf9c22e322ca5d2d80aa16e2f7281786
fadiSc <fadi_tawbi@hotmail.com>
2022-01-20 15:48:33 +0200

    Added Product in GridList + Changed Colors

--- Supermarket Application/src/supermarket/application/Cashier.java
+++ Supermarket Application/src/supermarket/application/Cashier.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
@@ -31,7 +32,8 @@ public class Cashier extends Application {
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
         
-        //creating left panel with items purchases 
+        //Creating Left Panel with items purchases 
+        
         HBox purchase = new HBox();
         VBox items = new VBox();
         VBox quantity = new VBox();
@@ -39,12 +41,12 @@ public class Cashier extends Application {
         Label lb1 = new Label("Items");
         Label lb2 = new Label("Quantity");
         Label lb3 = new Label ("Total");
-        lb1.setTextFill(Color.GREEN);
-        lb2.setTextFill(Color.GREEN);
-        lb3.setTextFill(Color.GREEN);
-        lb1.setStyle("-fx-background-color : pink ;-fx-border-color : black");
-        lb2.setStyle("-fx-background-color : pink ;-fx-border-color : black");
-        lb3.setStyle("-fx-background-color : pink ;-fx-border-color : black");
+//        lb1.setTextFill(Color.);
+//        lb2.setTextFill(Color.GREEN);
+//        lb3.setTextFill(Color.GREEN);
+        lb1.setStyle("-fx-background-color : teal ;-fx-border-color : black");
+        lb2.setStyle("-fx-background-color : teal ;-fx-border-color : black");
+        lb3.setStyle("-fx-background-color : teal ;-fx-border-color : black");
 
         lb1.setPrefSize(75, 75);
         lb2.setPrefSize(75, 75);
@@ -68,12 +70,64 @@ public class Cashier extends Application {
         price.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.DASHED, null , null)));
         purchase.getChildren().addAll(items,quantity,price);
         
+        //Creating Middle interface with the products showcase 
         
-        //creating bottom interface for control buttons
+        VBox itemspanel = new VBox();
+        GridPane gridlist = new GridPane();
+        HBox searchbar = new HBox();
+        Button searchbtn = new Button("Search");
+        TextField search = new TextField();
+        search.setPrefWidth(300);
+        searchbar.setPadding (new Insets (10,0,10,0));
+        gridlist.setPadding(new Insets (0,10,10,10));
+        
+        
+        Button[] categories = new Button[20];
+        
+        Button[] products = new Button[20];
+        
+        
+       //Filling Categories in Girdlist
+       
+       for (int i = 0 ; i < categories.length;i++){
+           
+           categories[i]= new Button("Product " +(i+1));
+           
+                if (i<5){
+                    
+                gridlist.add(categories[i],i , 0);
+              }
+                if (5 <= i && i<10){
+        
+                gridlist.add(categories[i],i-5 ,1);
+              }
+                
+               if (10 <= i && i<15){
+                    
+                gridlist.add(categories[i],i-10 , 2);
+              }
+                if (15 <= i && i<=20){
+        
+                gridlist.add(categories[i],i-15 ,3);
+              }
+                
+                
+             categories[i].setPrefSize(110, 100);
+       }
+        
+        searchbar.getChildren().addAll(searchbtn,search);
+        itemspanel.getChildren().addAll(searchbar,gridlist);
+        
+        
+        
+        
+        
+        //Creating Bottom Interface for control buttons
+        
         HBox bpanel = new HBox();
         Label lb4 = new Label("SubTotal");
         lb4.setPrefSize(155, 50);
-        lb4.setStyle("-fx-background-color : pink ;-fx-border-color : black");
+        lb4.setStyle("-fx-background-color : teal ;-fx-border-color : black");
 
         
         lb4.setAlignment(Pos.CENTER);
@@ -92,17 +146,13 @@ public class Cashier extends Application {
         bt2.setPrefSize(230, 75);
         bt3.setPrefSize(230, 75);
         
-        bt1.setStyle("-fx-background-color : chartreuse ;-fx-border-color : black");
-        bt2.setStyle("-fx-background-color: chartreuse ;-fx-border-color:black");
-        bt3.setStyle("-fx-background-color: chartreuse ;-fx-border-color:black");
+//        bt1.setStyle("-fx-background-color : chartreuse ;-fx-border-color : black");
+//        bt2.setStyle("-fx-background-color: chartreuse ;-fx-border-color:black");
+//        bt3.setStyle("-fx-background-color: chartreuse ;-fx-border-color:black");
         
         
-        // Creating bottom control panel 
-        
-        
-        
-        Button bt4 = new Button("test 1");
-        Button bt5 = new Button("test 2");
+        // Creating Bottom Control panel 
+
         HBox controlbtns = new HBox();
        
         controlbtns.getChildren().add(bt3);
@@ -114,6 +164,7 @@ public class Cashier extends Application {
         
         root.setLeft(purchase);
         root.setBottom(bpanel);
+        root.setCenter(itemspanel);
         Scene scene = new Scene(root, 800, 600);
         
         primaryStage.setTitle("Cashier");
