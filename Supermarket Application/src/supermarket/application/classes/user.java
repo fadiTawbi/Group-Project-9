c1f7e55acaa028d374cb3860d1467a03dd92ca27
mkk2000 <manuel_kolanjian@hotmail.com>
2022-01-23 19:51:07 +0200

    user  class created

---
+++ Supermarket Application/src/supermarket/application/classes/user.java
@@ -0,0 +1,45 @@
+
+package supermarket.application.classes;
+
+/**
+ *
+ * @author Manuel
+ */
+public class user {
+    private String username;
+    private String id;
+    private String pass;
+
+    public user(String username, String id, String pass) {
+        this.username = username;
+        this.id = id;
+        this.pass = pass;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+    
+}
