9304a16fab1c466c525858f7de0325842687be10
mkk2000 <manuel_kolanjian@hotmail.com>
2022-01-29 23:20:41 +0200

    connect to db final update

--- Supermarket Application/src/supermarket/application/classes/ConnectToDb.java
+++ Supermarket Application/src/supermarket/application/classes/ConnectToDb.java
@@ -3,46 +3,92 @@ package supermarket.application.classes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import supermarket.application.CreateAccount;
 
 /**
  *
  * @author scorp
  */
 public class ConnectToDb {
-       public void ConnectToDB() throws ClassNotFoundException, SQLException {
-        String host = "localhost";
-        String username = "root";
-        String rootPassword = "";
-
-        // Load the JDBC driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        System.out.println("Driver loaded");
-
-        // Establish a connection
-        Connection connection = DriverManager
-                .getConnection("jdbc:mysql://" + host + "/local_database?"
-                        + "user=" + username + "&password=" + rootPassword);
-
-        System.out.println("Database connected");
-
-           // Prepare a statement
-           Statement statement = connection.createStatement();
+    private static ConnectToDb _connectToDb = null;
+    
+    String host = "localhost";
+    String username = "root";
+    String rootPassword = "";
+    String connectionString="jdbc:mysql://"+host+"/local_database";
+    
+    private ConnectToDb(){
         
-        
-        String query = "Select Name From Product ";
-        ResultSet result =statement.executeQuery(query);
-        
-        for (int i =0; i < result.getFetchSize();i++){
-        System.out.println(result.next());
-        
-        }
-        
-        connection.close();
-        
-        System.out.println("Connection to database closed");
     }
     
+    public static ConnectToDb getInstance(){
+        if(_connectToDb==null){
+            _connectToDb=new ConnectToDb();
+            init();
+        }
+        return _connectToDb;
+    }
+    
+    public static void init(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectToDb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+       
+  
+        // Load the JDBC driver
+
+        // Establish a connection
+       return DriverManager.getConnection(connectionString,username,rootPassword);
+
+    }
+    
+    public void executeStatement(Connection connection, String statementStr){
+        
+        try{
+            connection = getConnection();
+            
+            Statement statement = connection.createStatement();
+            
+            statement.executeUpdate(statementStr);
+        }
+        catch(ClassNotFoundException ex1){
+            
+        }
+        catch(SQLException ex2){
+            
+        }
+        
+    }
+    
+    public ResultSet executeQuery(Connection connection, String query){
+        try{
+            connection = getConnection();
+            Statement statement = connection.createStatement();
+            return statement.executeQuery(query);
+        }
+        catch(ClassNotFoundException ex1){
+            
+        }
+        catch(SQLException ex2){
+            
+        }
+        return null;
+    }
+    
+    
+       
+   
 }
